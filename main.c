e1c31ad8a7969c3320fdcd76a43f0ebf2a645df1
Kenan Ozdemir <k.ozdemir@student.uliege.be>
2019-03-09 14:32:36 +0100

    Replace main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 #include "tries.h"
 #include "dictionary.h"
+#include "grid.h"
 
 int main(int argc, char** argv){
 
@@ -10,18 +11,25 @@ int main(int argc, char** argv){
         printf("Usage: %s <dictionary file path> <word puzzle path>", argv[0]);
         return EXIT_FAILURE;
     }
-    
+
     Node* dictionary = initDictionary(argv[1]);
     if(!dictionary) return EXIT_FAILURE;
+
+    Grid* grid = initGrid(argv[2]);
+    if(!grid) return EXIT_FAILURE;
+
+    findAllWords(grid,dictionary);
+
     char tmpWord[200] = {'\0'};
     FILE* fp = fopen("newDico.txt", "w");
-    printTrie(dictionary, tmpWord, 200, 0, fp);
+    if(!fp){
+        printf("File can't be opened.\n");
+        return EXIT_FAILURE;
+    }
+    printTrie(grid->found, tmpWord, 200, 0, fp);
     fclose(fp);
-    printf("%d\n",isWordInTrie(dictionary, "IBIS", 0));
-    printf("%d\n",isWordInTrie(dictionary, "IBISS", 0));
-    printf("%d\n",isWordInTrie(dictionary, "KAKI", 0));
 
     destroyTrie(dictionary);
-
+    destroyGrid(grid);
     return EXIT_SUCCESS;
 }
