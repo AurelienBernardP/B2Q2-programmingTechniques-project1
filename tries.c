27a43d1c83f72c365df1c75e58e82e1b9510ff2a
Aurélien Bernard <aurelienbernardp@hotmail.com>
2019-03-15 11:11:54 +0100

    modified the fuction arguments for simpler use and added specifications

--- tries.c
+++ tries.c
@@ -1,9 +1,21 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <ctype.h>
 
 #include "tries.h"
 
+const size_t MAX_WORD = 200;
+
+/*-------------------------------------------------------------------
+*  STRUCTURE node_t:
+*  This structure represents a node of the Ternary Search Trie.
+*
+*  FIELDS :
+*     - letter: the information contained in the cell
+*     - isEndOfWord : bolean value to symbolize the end of a word in the TST
+*     - left, right, middle : pointers to the child of the current node.
+--------------------------------------------------------------------*/
 struct node_t{
     char letter;
     bool isEndOfWord;
@@ -16,11 +28,12 @@ Node* initTrie(void){
     return NULL;
 }
 
-char obtLetter(Node* node){
-    return node->right->letter;
-}
-
-void printTrie(Node* head, char* word, size_t wordSize, size_t index, FILE* stream){
+/*********************
+ * printTrieHelper
+ * Helper function to the printTrie funtion defined in this module;
+ * 
+ * *******************/
+static void printTrieHelper(Node* head, char* word, size_t wordSize, size_t index, FILE* stream){
     if(!stream || !word || wordSize < index){
         fprintf(stderr, "Error uninitialized arguments or word too long");
         return;
@@ -33,19 +46,25 @@ void printTrie(Node* head, char* word, size_t wordSize, size_t index, FILE* stre
     }
 
     if(head->left != NULL){
-        printTrie(head->left, word, wordSize, index, stream);// we DO NOT put the letter in the word and get to the left;
+        printTrieHelper(head->left, word, wordSize, index, stream);// we DO NOT put the letter in the word and get to the left;
     }
     if(head->middle != NULL){
         word[index] = head->letter;
         ++index;
-        printTrie(head->middle, word, wordSize, index, stream);// we DO put the letter in the word and get to the middle;
+        printTrieHelper(head->middle, word, wordSize, index, stream);// we DO put the letter in the word and get to the middle;
         --index;
     }
     if(head->right != NULL){
-        printTrie(head->right, word, wordSize, index, stream);// we DO NOT put the letter in the word and get to the left;
+        printTrieHelper(head->right, word, wordSize, index, stream);// we DO NOT put the letter in the word and get to the left;
     }
     return;
 }
+void printTrie(Node* head, FILE* stream){
+    char tmpWord[MAX_WORD];
+    printTrieHelper(head, tmpWord, MAX_WORD, 0, stream);
+    return;
+}
+
 
 static Node* newNode(char newLetter, bool isEnd){
     Node* newNode = malloc(sizeof(Node));
@@ -57,10 +76,15 @@ static Node* newNode(char newLetter, bool isEnd){
     return newNode;
 }
 
-Node* insertWord(Node* head, char* word, size_t index){
+/*********************
+ * insertWordHelper
+ * Helper function to the insertWord funtion defined in this module;
+ * 
+ * *******************/
+static Node* insertWordHelper(Node* head, char* word, size_t index){
     if(!word) return head;
 
-    char newLetter = word[index];
+    char newLetter = tolower(word[index]);
     if(newLetter == '\n')
         newLetter == '\0';
 
@@ -70,43 +94,52 @@ Node* insertWord(Node* head, char* word, size_t index){
     if(!head){
         return head;
     }
-
-
-
     
     if(newLetter < head->letter) // on va a chaque fois verifier les 3 conditions -> à changer
-        head->left = insertWord(head->left, word, index);
+        head->left = insertWordHelper(head->left, word, index);
     if(newLetter > head->letter)
-        head->right = insertWord(head->right, word, index);
+        head->right = insertWordHelper(head->right, word, index);
     if(newLetter == head->letter){    
         if(word[index+1] == '\n' || word[index+1] == '\0'){
             head->isEndOfWord = true;
             return head;
         }
-        head->middle = insertWord(head->middle, word, ++index);
+        head->middle = insertWordHelper(head->middle, word, ++index);
     }
     return head;
 }
+Node* insertWord(Node* head, char* word){
+    return insertWordHelper(head, word, 0);
+}
 
-bool isWordInTrie(Node* head, char* word, size_t index){
+
+/*********************
+ * isWordInTrieHelper
+ * Helper function to the isWordInTrie funtion defined in this module;
+ * 
+ * *******************/
+static bool isWordInTrieHelper(Node* head, char* word, size_t index){
     if(!head || !word)
         return false;
     
-    char letter = word[index];
+    char letter = tolower(word[index]);
     if((('\0' == letter) && ('\0'== head->letter)) || (word[index+1] == '\0' && head->isEndOfWord && word[index] == head->letter))
         return true;
 
     if(letter < head->letter){
-        return isWordInTrie(head->left, word, index);
+        return isWordInTrieHelper(head->left, word, index);
     }
     if(letter > head->letter){
-        return isWordInTrie(head->right, word, index);
+        return isWordInTrieHelper(head->right, word, index);
     }
     if(letter == head->letter){
-        return isWordInTrie(head->middle, word, ++index);
+        return isWordInTrieHelper(head->middle, word, ++index);
     }
     
 return true;///will never reach this condition
+}
+bool isWordInTrie(Node* head, char* word){
+    return isWordInTrieHelper(head, word, 0);
 }
 
 void destroyTrie(Node* head){
--- tries.h
+++ tries.h
@@ -1,21 +1,91 @@
+/********************************
+ * Module Handling the creation, modification
+ * and deletion of Ternary Search Trie structure.
+ * ******************************/
+
 #ifndef __TRIES_HANDLER__
 #define __TRIES_HANDLER__
 
 #include <stdbool.h>
 #include <stdlib.h>
 
+/* Opaque Structure */
 typedef struct node_t Node;
 
+/* ------------------------------------------------------------------------- *
+ * Creates a new empty Ternary Search Trie (TST)object.
+ *
+ * PARAMETERS
+ * Void, no parameters
+ *
+ * RETURN
+ * Node pointer to a new TSt
+ *
+ * USE:
+ * 
+ *      Node* TSTNAME = initTrie();
+ * ------------------------------------------------------------------------- */
 Node* initTrie(void);
 
-char obtLetter(Node* node);
+/* ------------------------------------------------------------------------- *
+ * Inserts a word in an previously created TST.
+ *
+ * PARAMETERS
+ * head : Node pointer to the root of the peviously created TST
+ * word : The word to be inserted.
+ *         !! The format of the word must be a character array with '\n' or '\0'
+ *            as a end of word charachter !!
+ * 
+ * RETURN
+ * Node pointer to the TST with the new word inserted.
+ *
+ * USE:
+ *       PreviouslyCreatedTSTPointer = insertWord(PreviouslyCreatedTSTPointer, word);
+ * ------------------------------------------------------------------------- */
+Node* insertWord(Node* head, char* word);
 
-Node* insertWord(Node* head, char* word, size_t index);
+/* ------------------------------------------------------------------------- *
+ * Prints all the words in lower case contained in a TST, into the wanted file stream.
+ *
+ * PARAMETERS
+ * head : Node pointer to the root of the peviously created TST
+ * stream : Pointer to a valid file stream, standard or previously opened in write("w") mode
+ * 
+ * RETURN
+ * void, the word will be printed in the file stream.
+ *
+ * USE: with for example the standard outpout file stream
+ *       printTrie(PreviouslyCreatedTSTPointer, stdout);
+ * ------------------------------------------------------------------------- */
+void printTrie(Node* head, FILE* stream);
 
-void printTrie(Node* head, char* word, size_t wordSize, size_t index, FILE* stream);
+/* ------------------------------------------------------------------------- *
+ * Lookup for a word in a previously created TST. 
+ *
+ * PARAMETERS
+ * head : Node pointer to the root of the peviously created TST
+ * word : The word to be looked up.
+ *         !! The format of the word must be a character array with '\n' or '\0'
+ *            as a end of word charachter !!
+ * RETURN
+ * 0 if the word is NOT in the TST
+ * 1 if the word IS in the TST
+ *
+ * ------------------------------------------------------------------------- */
+bool isWordInTrie(Node* head, char* word);
 
-bool isWordInTrie(Node* head, char* word, size_t index);
-
+/* ------------------------------------------------------------------------- *
+ * Frees all the cells in the previously created TST.
+ *
+ * PARAMETERS
+ * head : Node pointer to the root of the peviously created TST
+ * 
+ * RETURN
+ * void, the TST is destroyed
+ *
+ * USE: 
+ *       destroyTrie(PreviouslyCreatedTSTPointer);
+ * ------------------------------------------------------------------------- */
 void destroyTrie(Node* head);
 
 #endif
