3085b16f10a5c6d2c1671abc3caa9c769bc7ef7a
Kenan Ozdemir <k.ozdemir@student.uliege.be>
2019-03-20 21:35:31 +0100

    Ajout du temps passé dans les différentes fonctions

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "tries.h"
 #include "dictionary.h"
@@ -11,21 +12,31 @@ int main(int argc, char** argv){
         printf("Usage: %s <dictionary file path> <word puzzle path>\n", argv[0]);
         return EXIT_FAILURE;
     }
+    clock_t start, end, end1, end2, end3;
+    start = clock();
+
 
     Root* dictionary = initDictionary(argv[1]);
     if(!dictionary) return EXIT_FAILURE;
+    end1 = clock();
+    printf("Init dict = %lf \n", ((double)  (end1 - start)) / CLOCKS_PER_SEC);
 
     Grid* grid = initGrid(argv[2]);
     if(!grid) return EXIT_FAILURE;
+    end2 = clock();
+    printf("Init grid = %lf \n", ((double)  (end2 - start)) / CLOCKS_PER_SEC);
 
     findAllWords(grid,dictionary);
+    end3 = clock();
+    printf("Parcours = %lf \n", ((double)  (end3 - start)) / CLOCKS_PER_SEC);
 
-
-    printTrie(grid->found, stdout);
-
+    printTrie(getWordsFound(grid), stdout);
 
     destroyFromRoot(dictionary);
     destroyGrid(grid);
     
+    end = clock();
+    printf("Time spent = %lf \n", ((double)  (end - start)) / CLOCKS_PER_SEC);
+
     return EXIT_SUCCESS;
 }
